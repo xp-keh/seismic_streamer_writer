b78f8d6e24b416d89015efc9f285699c42213e5c
ernestbenedictus <m010d4ky1623@bangkit.academy>
2025-04-24 02:09:24 +0700

    seismic writer streamer

---
+++ consume/kafka.py
@@ -0,0 +1,62 @@
+import json
+import logging
+from aiokafka import AIOKafkaConsumer
+from config.logging import Logger
+from datastore.redis_store import save_seismic_data
+from consume.websocket_manager import WebSocketManager
+import traceback
+from starlette.websockets import WebSocketDisconnect
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+class AsyncConsumer:
+    def __init__(self, kafka_broker, topic, group_id, websocket_manager: WebSocketManager):
+        self.kafka_broker = kafka_broker
+        self.topic = topic
+        self.group_id = group_id
+        self.logger = Logger().setup_logger(service_name="consumer")
+        self.consumer = AIOKafkaConsumer(
+            self.topic,
+            bootstrap_servers=self.kafka_broker,
+            group_id=self.group_id,
+            auto_offset_reset="latest",
+            value_deserializer=lambda m: json.loads(m.decode("utf-8")),
+        )
+        self.websocket_manager = websocket_manager
+
+    async def start(self):
+        await self.consumer.start()
+
+    async def stop(self):
+        await self.consumer.stop()
+
+    async def consume(self):
+        """Continuously consume messages, save them to Redis, and put them in the queue for SSE."""
+        try:
+            async for message in self.consumer:
+                raw_data = message.value
+
+                seismic_data = {
+                    "dt": int(str(raw_data.get("dt", 0))[:10]),
+                    "network": raw_data.get("network", "unknown"),
+                    "station": raw_data.get("station", "unknown"),
+                    "channel": raw_data.get("channel", "unknown"),
+                    "data": raw_data.get("data", 0.0)
+                }
+
+                key = f"seismic:{seismic_data['timestamp']}_{seismic_data['location']}"
+
+                await save_seismic_data(key, seismic_data)
+                try:
+                    await self.websocket_manager.broadcast(json.dumps(seismic_data))
+                except WebSocketDisconnect:
+                    self.logger.warning("WebSocket disconnected. Skipping message broadcast.")
+
+                try:
+                    await self.consumer.commit()
+                except Exception as e:
+                    self.logger.error(f"Error committing Kafka offset: {e}")
+                    
+        except Exception as e:
+            self.logger.error(f" [x] Error in consumer: {e}")
+            self.logger.error(traceback.format_exc())
---
+++ writer/clickhouse_writer.py
@@ -0,0 +1,84 @@
+import logging
+import clickhouse_connect
+import pytz
+from datetime import datetime, timezone
+from config.utils import get_env_value
+from datastore.redis_store import get_all_seismic_data, clear_redis
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+CLICKHOUSE_HOST = get_env_value("CLICKHOUSE_HOST")
+CLICKHOUSE_DATABASE = get_env_value("CLICKHOUSE_DATABASE")
+CLICKHOUSE_USER = get_env_value("CLICKHOUSE_USER")
+CLICKHOUSE_PASSWORD = get_env_value("CLICKHOUSE_PASSWORD")
+
+gmt7 = pytz.timezone("Asia/Jakarta")
+
+def format_unix_to_gmt7_string(unix_ts):
+    try:
+        dt_utc = datetime.utcfromtimestamp(unix_ts)
+        dt_gmt7 = dt_utc.replace(tzinfo=pytz.utc).astimezone(gmt7)
+        return dt_gmt7.strftime("%d-%m-%YT%H:%M:%S")
+    except Exception:
+        return ""
+    
+def safe_float(val: str) -> float:
+    try:
+        return float(val.replace(",", "."))
+    except (ValueError, AttributeError):
+        return 0.0 
+
+async def bulk_write_to_clickhouse():
+    try:
+        clickhouse_client = clickhouse_connect.get_client(
+            host=CLICKHOUSE_HOST,
+            port=8123,
+            username=CLICKHOUSE_USER,
+            password=CLICKHOUSE_PASSWORD,
+            database=CLICKHOUSE_DATABASE
+        )
+    except Exception as e:
+        logging.error(f"Failed to connect to ClickHouse: {e}")
+        return
+
+    data = await get_all_seismic_data()
+    
+    if not data:
+        logging.info("No new data to write to ClickHouse.")
+        return
+
+    timestamp_str = datetime.now().strftime("%Y%m%d_%H%M")
+    table_name = f"seismic_{timestamp_str}"
+
+    create_table_query = f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        dt String,
+        network String,
+        station String,
+        channel String,
+        data Float32
+    ) ENGINE = MergeTree()
+    ORDER BY timestamp
+    """
+    clickhouse_client.command(create_table_query)
+
+    data_to_insert = [
+        (
+            row["dt"],
+            row["network"],
+            row["station"],
+            row["channel"],
+            safe_float(row.get("data", 0.0)),
+        )
+        for row in data
+    ]
+
+    utc_now = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S GMT+0")
+    logging.info(f"Uploading {len(data)} records to ClickHouse at {utc_now}")
+
+    clickhouse_client.insert(table_name, data_to_insert)
+
+    await clear_redis()  
+    logging.info(f"Uploaded {len(data)} records to ClickHouse and cleared Redis.")
+
+    clickhouse_client.close()
